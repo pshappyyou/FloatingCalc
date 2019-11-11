8e22e68b80a5119897d35bc90d6e946079f5d164
Daniel Park <pshappyyou@gmail.com>
2019-11-11 15:49:50 +1100

    Add files via upload

---
+++ FloatingCalc/Modules/FC_main.py
@@ -0,0 +1,20 @@
+import sys
+
+import PyQt5
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+
+CalUI = '../_uiFiles/FCUI.ui'
+
+class MainDialog(QDialog):
+    def __init__(self):
+        QDialog.__init__(self, None)
+        uic.loadUi(CalUI, self)
+
+
+app = QApplication(sys.argv)
+main_dialog = MainDialog()
+main_dialog.show()
+app.exec_()
